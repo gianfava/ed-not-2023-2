70e9f0032864163dc3c72e6c4a7d2f01de58cdc5
Gian Fava <49363295+gianfava@users.noreply.github.com>
2023-08-11 21:59:47 -0300

    (11/08) Funções

---
+++ 01_funcoes.py
@@ -0,0 +1,11 @@
+def calc_imc(peso, altura):
+    return peso / altura ** 2
+
+
+
+# p = 80
+# a = 1.75
+# imc = calc_imc(p,a)
+# print (' O seu imc é: ',  round(imc,1))
+
+print (calc_imc(80,1.75))
