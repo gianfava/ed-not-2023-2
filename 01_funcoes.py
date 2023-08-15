6cb7947601583e4f9cfc87db72aa8b2e51c7b6c8
Fausto G. Cintra <professor@faustocintra.com.br>
2023-08-14 22:13:58 -0300

    (14/08) Início de listas

--- 01_funcoes.py
+++ 01_funcoes.py
@@ -9,3 +9,27 @@ def calc_imc(peso, altura):
 # print(imc)
 
 print(calc_imc(85, 1.72))
+
+################################################
+
+from math import pi
+
+"""
+    Função que calcula a área de uma figura geométrica plana
+"""
+def calc_area(base, altura, tipo):
+    if tipo == "R":     # Retângulo
+        resultado = base * altura
+    elif tipo == "T":   # Triângulo
+        resultado = base * altura / 2
+    elif tipo == "E":   # Elipse
+        resultado = (base / 2) * (altura / 2) * pi
+    else:   # Forma inválida ou desconhecida
+        resultado = None
+
+    return resultado
+
+print("Retângulo 20x30: ", calc_area(20, 30, "R"))
+print("Triângulo 45x32: ", calc_area(45, 32, "T"))
+print("Elipse 10x15: ", calc_area(10, 15, "E"))
+print("Desconhecido 12x16: ", calc_area(12, 16, "X"))
